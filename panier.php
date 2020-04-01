abb3e4946cabab9b12152499959e7e6ad8ceb729
maryem10 <meriammeri.mm@gmail.com>
2020-04-01 22:33:52 +0100

    panier modofication

--- panier.php
+++ panier.php
@@ -46,6 +46,72 @@ input[type=reset]  {
     /* transform: rotate(45deg); */
 
   }
+  .con {
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.con input {
+  position: absolute; */
+    opacity: 1;
+    /* cursor: pointer; */
+    height: 30px;
+    width: 0;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+    /* top: 0; */
+    /* left: 0; */
+    height: 25px;
+    width: 25px;
+    background-color: #eee;
+}
+
+/* On mouse-over, add a grey background color */
+.con:hover input ~ .checkmark {
+  background-color: #ccc;
+}
+
+/* When the checkbox is checked, add a blue background */
+.con input:checked ~ .checkmark {
+  background-color: #ec971f;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.con input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.con .checkmark:after {
+  left: 9px;
+  top: 5px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
 
 
 </style>
@@ -72,7 +138,10 @@ input[type=reset]  {
 							<tbody>
 								<tr>
 									<td>
-										<input type="checkbox" name="select">
+										<label class="con">
+										  <input type="checkbox" checked="checked">
+										  <span class="checkmark"></span>
+										</label>
 									</td>
 									<td>
 										<img style=" width: 50px;" src="images/huile1.png">
@@ -98,7 +167,10 @@ input[type=reset]  {
 							<tbody>
 								<tr>
 									<td>
-									<input type="checkbox" class="custom-control-input" id="check1" checked>
+									<label class="con">
+										  <input type="checkbox" checked="checked">
+										  <span class="checkmark"></span>
+									</label>
 									</td>
 									<td>
 										<img style=" width: 50px;" src="images/nivea1.jpg">
